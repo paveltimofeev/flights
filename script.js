8b49305f6df5905f776ffa3ae0f2978303af6ec8
Pavel Timofeev <paveltimofeev@users.noreply.github.com>
2022-06-15 21:05:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+const columns = document.querySelector('.columns');
+
+const info = document.querySelector('#info');
+info.innerText = 
+`Prices ${data.stats.minPrice} - ${data.stats.maxPrice} ${data.params.currency.toUpperCase()}. ` + 
+`Travel time ${Math.round(data.stats.minDuration/60)} - ${Math.round(data.stats.maxDuration/60)} hours. ` + 
+`Checked ${data.stats.totalRequests} variants. ` + 
+`For ${data.params.dateRange.days} days since ${data.params.dateRange.since.split('T')[0]}. ` + 
+`Scan date: ${data.date}.`;
+
+function onCellClick (cellEl, idx) {
+  console.log(data.results[idx]);
+  window.open('https://www.aviasales.ru/' + data.results[idx].link, '_blank').focus();
+  cellEl.classList.add('cell--clicked');
+}
+
+function renderRowHeaders (rowsHeaders) {
+
+  const col = document.createElement('div');
+  col.classList.add('col');
+  col.classList.add('col-row-header');
+  Object.keys(rowsHeaders).forEach(row => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.classList.add('cell--row-header');
+    cell.innerText = row;
+    col.appendChild(cell);
+  });
+
+  const footer = document.createElement('div');
+  footer.classList.add('cell');
+  footer.classList.add('cell--row-header');
+  footer.classList.add('cell--footer');
+  col.appendChild(footer);
+  columns.appendChild(col);
+}
+
+function render (cells, footerLabel, weekEnd) {
+  
+  const col = document.createElement('div');
+  col.classList.add('col');
+  if (weekEnd) {
+    col.classList.add('we');
+  }
+
+  cells.forEach(c => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    if (c.idx) {
+      cell.setAttribute('data-idx', c.idx);
+      cell.addEventListener('click', () => onCellClick(cell, c.idx));
+    }
+
+    if (c.tooltip) {
+      cell.setAttribute('data-tippy-content', c.tooltip);
+    }
+
+    if (c.minPrice) {
+      cell.classList.add('cell--min');
+    }
+
+    if (c.quickest) {
+      cell.classList.add('cell--quic');
+    }
+
+    if (c.transfers === 0) {
+      cell.classList.add('cell--t0');
+    }
+
+    if (c.transfers === 1) {
+      cell.classList.add('cell--t1');
+    }
+
+    const r = document.createElement('span');
+    r.classList.add('r');
+    r.classList.add('s' + c.s );
+    r.classList.add('c' + c.c);
+
+    cell.appendChild(r);
+    col.appendChild(cell);
+  });
+
+  const footer = document.createElement('div');
+  footer.classList.add('cell');
+  footer.classList.add('cell--footer');
+  const footerSpan = document.createElement('span');
+  footerSpan.innerText = footerLabel;
+  footer.appendChild(footerSpan);
+  col.appendChild(footer);
+  
+  columns.appendChild(col);
+}
+
+const sortByDate = (a, b) => {
+  const d1 = (new Date(a.departure_at.split('T')[0])).getTime();
+  const d2 = +(new Date(b.departure_at.split('T')[0])).getTime();
+  return d1 - d2;
+}
+
+data.results.sort(sortByDate);
+
+const cols = {};
+data.results.forEach((record, idx) => {
+  record.idx = idx;
+  const footerText = record.departure_at.split('T')[0];
+  if(cols[footerText]) {
+    cols[footerText].push(record);
+  }
+  else {
+    cols[footerText] = [record];
+  }
+});
+
+const rowsIndexMap = {};
+let index = 0;
+for(let i = 0; i < data.params.from.length; i++) {
+  for(let j = 0; j < data.params.to.length; j++) {
+    rowsIndexMap[data.params.from[i] + '-' + data.params.to[j]] = index;
+    index++;
+  } 
+}
+
+renderRowHeaders(rowsIndexMap);
+
+const lowPrice = data.params?.priceRange?.lowPrice || 300;
+const priceStep = data.params?.priceRange?.step || 100;
+const maxRows = data.params.from.length * data.params.to.length;
+Object.keys(cols).forEach(date => {
+  const col = cols[date];
+  const cells = [];
+  for(let i = 0; i < maxRows; i++) {
+    cells.push({r: 0, s: 0});
+  }
+  col.forEach(c => {
+
+    const i = rowsIndexMap[c.route];
+
+    cells[i].idx = c.idx;
+    cells[i].c = Math.round( (c.duration - data.stats.minDuration) / (data.stats.maxDuration - data.stats.minDuration) * 5 + 1 );
+    cells[i].c = cells[i].c > 5 ? 5 : cells[i].c;
+
+    // cells[i].s = Math.round( (c.price - data.stats.minPrice) / (data.stats.maxPrice - data.stats.minPrice) * 5 + 1 );
+    const priceLevel = Math.round((c.price - lowPrice) / priceStep);
+    cells[i].s = priceLevel <= 0 ? 1 : priceLevel > 5 ? 5 : priceLevel;
+
+    const isMinPrice = c.price < data.stats.minPrice * 1.2;
+    const isQuickest = c.duration < data.stats.minDuration * 1.2;
+    cells[i].minPrice = isMinPrice;
+    cells[i].quickest = isQuickest;
+    cells[i].transfers = c.transfers;
+
+    const priceLabel = !isMinPrice ? `Price` : `BEST Price`;
+    const durationLabel = !isQuickest ? `Time` : `BEST Time`;    
+    cells[i].tooltip = priceLabel + ` ${c.price}` + ` | ` + durationLabel + ` ${Math.round(c.duration/60)}H` + ` | ⇄ ` + c.transfers;
+  });
+
+  const dayOfWeek = (new Date(date)).getDay();
+  const weekEnd = dayOfWeek === 6 || dayOfWeek === 0;
+  render(cells, date, weekEnd);
+});
+
+tippy('[data-tippy-content]', {theme: 'light'});
